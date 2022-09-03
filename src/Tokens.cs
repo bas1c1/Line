7d8c73ac010a552ccaef4418b406bb9b3befb878
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-09-03 16:42:00 +0300

    Line

---
+++ src/Tokens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lines
+{
+    public enum Tokens
+    {
+        NONE,
+        WORD,
+        LBRACE,
+        RBRACE,
+        NUM,
+        STRING,
+        PLUS,
+        MINUS,
+        STAR,
+        SLASH,
+        EQ,
+        VAR,
+        DOTDOT
+    }
+}
