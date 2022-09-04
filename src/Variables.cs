33e3543ff1fbd0b90e44c3624643acca5fb6359c
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-09-04 16:37:34 +0300

    Lines

---
+++ src/Variables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lines
+{
+    public static class Variables
+    {
+        public static Dictionary<string, object> vars = new Dictionary<string, object>();
+    }
+}
